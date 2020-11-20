e49a5c645cb3ed5cfe1b7270f630b1c169796a67
Shubham Parihar <shubhamparihar391@gmail.com>
2020-11-21 00:47:00 +0530

    added: BinarySearchTree.h

---
+++ BinarySearchTree.h
@@ -0,0 +1,69 @@
+// Node struct
+// usage: struct Node* root = NULL;
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// function declarations
+struct Node* createNode(int data);
+struct Node* insert(int data, struct Node** node);
+void printInOrder(struct Node* node);       // [left] [root] [right]
+void printPreOrder(struct Node* node);      // [root] [left] [right]
+void printPostOrder(struct Node* node);     // [left] [right] [root]
+
+// create node function
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// insert node function
+// usage: insert(1, &root);
+struct Node* insert(int data, struct Node** node) {
+    if (*node == NULL) {
+        *node = createNode(data);
+    } else if (data < (*node)->data) {
+        (*node)->left = insert(data, &(*node)->left);
+    } else if (data > (*node)->data) {
+        (*node)->right = insert(data, &(*node)->right);
+    }
+    return *node;
+}
+
+// inorder traversal function [Depth First Traversal]
+// usage: printInOrder(root);
+void printInOrder(struct Node* node) {
+    if (node == NULL) {
+        return;
+    }
+    printInOrder(node->left);
+    printf("%d ", node->data);
+    printInOrder(node->right);
+}
+
+// preOrder traversal function [Depth First Traversal]
+// usage: printPreOrder(root);
+void printPreOrder(struct Node* node) {
+    if (node == NULL) {
+        return;
+    }
+    printf("%d ", node->data);
+    printPreOrder(node->left);
+    printPreOrder(node->right);
+}
+
+// postOrder traversal function [Depth First Traversal]
+// usage: printPostOrder(root);
+void printPostOrder(struct Node* node) {
+    if (node == NULL) {
+        return;
+    }
+    printPostOrder(node->left);
+    printPostOrder(node->right);
+    printf("%d ", node->data);
+}
--- LinkedList.h
+++ LinkedList.h
@@ -1,9 +1,11 @@
-// structs
+// Node struct
 struct Node {
     int data;
     struct Node* next;
 };
 
+// LinkedList struct
+// usage: struct LinkedList list = {NULL, NULL, 0};
 struct LinkedList {
     struct Node* head;
     struct Node* tail;
@@ -11,15 +13,17 @@ struct LinkedList {
 };
 
 
-// function declaration
-struct Node* add(int data, struct LinkedList* list);
+// function declarations
+struct Node* push(int data, struct LinkedList* list);
 void print(struct LinkedList* list);
 int size(struct LinkedList* list);
 int peek(int position, struct LinkedList* list);
+struct Node* insert(int data, int position, struct LinkedList* list);
 
 
-// add method
-struct Node* add(int data, struct LinkedList* list) {
+// push function
+// usage: push(1, &list);
+struct Node* push(int data, struct LinkedList* list) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (!list->head) {
         list->head = newNode;
@@ -36,7 +40,8 @@ struct Node* add(int data, struct LinkedList* list) {
     return list->tail;
 }
 
-// print method
+// print function
+// usage: print(&list);
 void print(struct LinkedList* list) {
     struct Node* ptr = list->head;
     while (ptr) {
@@ -45,12 +50,14 @@ void print(struct LinkedList* list) {
     }
 }
 
-// size method
+// size function
+// usage: size(&list);
 int size(struct LinkedList* list) {
     return list->size;
 }
 
-// peek method
+// peek function
+// usage: peek(1, &list);
 int peek(int position, struct LinkedList* list) {
     struct Node* ptr = list->head;
     for (int i=0; i<position; i++) {
@@ -59,8 +66,9 @@ int peek(int position, struct LinkedList* list) {
     return ptr->data;
 }
 
-// push method
-struct Node* push(int data, int position, struct LinkedList* list) {
+// push function
+// usage: insert(1, 1, &list);
+struct Node* insert(int data, int position, struct LinkedList* list) {
     if (position <= list->size) {
         struct Node* previous;
         struct Node* current = list->head;
